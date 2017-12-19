cc1c5f678468491afd7f4b20ccaee71654b8808e
illingtonFlex <jason.norman@gmail.com>
2017-12-18 21:54:48 -0600

    Implementing command-line-args to set host and port.

--- serviceClients.js
+++ serviceClients.js
@@ -1,6 +1,15 @@
 'use strict';
 
 const Client = require('node-rest-client').Client;
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [
+  { name: 'host', type: String, multiple: false, defaultValue: "localhost" },
+  { name: 'port', type: String, multiple: false, defaultValue: "8080" }
+]
+const cloptions = commandLineArgs(optionDefinitions);
+
+let hostPort = "http://" + cloptions.host + ":" + cloptions.port;
 
 let client = new Client();
 
@@ -12,11 +21,11 @@ let args = {
     }
 };
 
-client.registerMethod("listValidWords", "http://localhost:8080/listValidWords", "GET");
-client.registerMethod("createMatch",    "http://localhost:8080/deduceMatch/createMatch", "POST");
-client.registerMethod("matchDetails",   "http://localhost:8080/deduceMatch/${match_id}/details", "GET");
-client.registerMethod("letterAtIndex",  "http://localhost:8080/deduceMatch/${match_id}/letterAtIndex/${index}", "GET");
-client.registerMethod("solve",          "http://localhost:8080/deduceMatch/${match_id}/solve/${solution}", "PUT");
+client.registerMethod("listValidWords", hostPort + "/listValidWords", "GET");
+client.registerMethod("createMatch",    hostPort + "/deduceMatch/createMatch", "POST");
+client.registerMethod("matchDetails",   hostPort + "/deduceMatch/${match_id}/details", "GET");
+client.registerMethod("letterAtIndex",  hostPort + "/deduceMatch/${match_id}/letterAtIndex/${index}", "GET");
+client.registerMethod("solve",          hostPort + "/deduceMatch/${match_id}/solve/${solution}", "PUT");
 
 module.exports = {
 
